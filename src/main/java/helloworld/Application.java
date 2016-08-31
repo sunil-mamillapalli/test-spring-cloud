c94be5076ae2978ac2dda4304763bde1152df019
Ravikumar Venkatesan <ravikumar.venkatesan@hpe.com>
2016-08-31 16:35:26 -0700

    updated manifest

--- src/main/java/helloworld/Application.java
+++ src/main/java/helloworld/Application.java
@@ -1,5 +1,5 @@
 package helloworld;
-
+//test message
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
